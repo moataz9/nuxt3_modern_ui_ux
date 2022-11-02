e2589fb85178af64d55bab6db70b68c5c41006c3
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-11-02 10:00:51 +0200

    Brand Component Container section

--- components/App/Brand.vue
+++ components/App/Brand.vue
@@ -1,13 +1,40 @@
-<script setup lang='ts'>
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    Brand
+  <div class="gpt3__brand section__padding">
+    <div>
+      <img src="~/assets/images/google.png" />
+    </div>
+    <div>
+      <img src="~/assets/images/slack.png" />
+    </div>
+    <div>
+      <img src="~/assets/images/atlassian.png" />
+    </div>
+    <div>
+      <img src="~/assets/images/dropbox.png" />
+    </div>
+    <div>
+      <img src="~/assets/images/shopify.png" />
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.gpt3__brand {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 
+  div {
+    flex: 1;
+    max-width: 150px;
+    min-width: 120px;
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
 </style>
